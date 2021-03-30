23a02a3a5fc4d63a04be59c31e849e7310462bb5
Casey Crouch <casejamesc@gmail.com>
2021-03-29 21:36:52 -0600

    Update issue urls

--- src/parser_exercise/ParserExercise.js
+++ src/parser_exercise/ParserExercise.js
@@ -1,24 +1,22 @@
-import React    from "react"
-import Exercise from "../exercise/Exercise"
+import React from "react";
+import Exercise from "../exercise/Exercise";
 
 const ParserExercise = () => {
   return (
     <div className="parser">
       <Exercise
-        solution = {<Solution />}
-        specsUrl = "https://github.com/CommissionAI/spiff_react_exercises/issues/2"
-        title    = "Parser Exercise"
+        solution={<Solution />}
+        specsUrl="https://github.com/SpiffInc/spiff_react_exercises/issues/2"
+        title="Parser Exercise"
       />
     </div>
-  )
-}
+  );
+};
 
-export default ParserExercise
+export default ParserExercise;
 
 // ----------------------------------------------------------------------------------
 
 const Solution = () => {
-  return (
-    <div>Add solution here</div>
-  )
-}
+  return <div>Add solution here</div>;
+};
